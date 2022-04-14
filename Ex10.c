3768c2f819de6df43a69cb12099049e3967cdc1d
Samuel Leal <samuelmarquessousaleal@gmail.com>
2022-04-14 14:27:36 -0300

    Finalizando
    Adicionando e organizando exercicios

---
+++ Ex10.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <locale.h>
+
+void adivinha() {
+    /*Faça um programa para adivinhar um número escolhido pelo usuário, entre 1 e 1023. Indique ao 
+    final quantas tentativas foram necessárias. O programa deverá localizar o número escolhido pelo 
+    usuário em no máximo 10 tentativas*/
+
+    setlocale(LC_ALL,"portuguese");
+    int aleatorio, ultimo=1023,primeiro=1,tentativas=0;
+    char simbolomate;
+
+    do{
+        aleatorio = primeiro + (ultimo-primeiro) /2;
+        tentativas ++;
+        printf("O número que você escolheu é maior,menor ou igual a %d? Digite '>', '<' ou '=' respectivamente\n Número de tentativas : %d\n",aleatorio,tentativas);
+        scanf(" %c",&simbolomate);
+        switch (simbolomate){
+            case '=':
+               printf("número correto");
+               break;
+            case '>':
+               primeiro = aleatorio + 1;
+               break;
+            case '<':
+               ultimo = aleatorio - 1;
+               break;
+        }
+    } while(simbolomate != '=');
+}
+
+int main()
+{
+    int numero, tentar_de_novo=1;
+    do{
+        printf("Pense e digite um número de 1 a 1023\n");
+        scanf("%d",&numero);
+        adivinha();
+        printf("\nSe você deseja jogar novamente digite um qualquer numero, se não digite 0\n");
+        scanf("%d",&tentar_de_novo);
+    }while(tentar_de_novo);
+    return 0;
+}
--- Ex9.c
+++ Ex9.c
@@ -6,18 +6,30 @@ int main()
 {
     /* srand(time(NULL)) inicializa o gerador de números aleatórios com o valor da função time(NULL). time (NULL) é o total de segundos passados
     desde 1 de janeiro de 1970 até a data atual. Cada execução faz com que o valor de referencia seja diferente.
-    programa para gerar números aleatórios */
-    setlocale(LC_ALL,"portugues");
-    int n;
-    printf("Gerando 10 valores aleatórios:\n");
+    programa para gerar números aleatórios 
+            ENTRADA                     SAIDA
+               0       Espera-se um  numero de vitorias aleatorio.
+    */
+    setlocale(LC_ALL,"portuguese");
+    int num, aleatorio, quant;
+    quant = 0;
+
+    printf("Escolha 0 para cara ou 1 para coroa:\n");
+    scanf("%i",&num);
     //srand(0);
     srand(time(NULL));
-    for (n = 0; n < 10; n++ ){
+
+    for (int n = 0; n < 10; n++ ){
         /* gerando valores aleatórios entre 1 e 100 */
-        printf("%d ", rand() % 100 +1);
-        
+        aleatorio = (rand() % 2);
+
+        if (num ==  aleatorio){
+            quant++;
+        }
     }
-    getch();    
+
+    printf("Voce ganhou %i vezes.", quant);
+
+    getch(); 
     return 0;
 }
-
